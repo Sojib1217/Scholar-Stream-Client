f577cb2e53c182a468b8ed73c6607ab68aba1a9d
Sojib1217 <sojibhasan1888@gmail.com>
2025-12-22 15:08:49 +0600

    server side filter funtionality added

--- src/Pages/All-Scholarships/AllScholarships.jsx
+++ src/Pages/All-Scholarships/AllScholarships.jsx
@@ -1,30 +1,31 @@
-
-
 import ScholarshipCard from '../../components/ScholarshipCard/ScholarshipCard';
-
 import { useQuery } from '@tanstack/react-query';
 import useAxiosSecure from '../../hooks/useAxiosSecure';
 import { useState } from 'react';
+import Loader from '../../components/Loader/Loader';
 
 
 const AllScholarships = () => {
     const axiosSecure = useAxiosSecure()
     const [searchText, setSearchText] = useState('')
-    const { data: scholarship = [], } = useQuery({
-        queryKey: ['scholarships',searchText],
+    const [filters,setFilters]=useState({degree: "",})
+    const { data: scholarship = [],isLoading } = useQuery({
+        queryKey: ['scholarships', searchText, filters],
         queryFn: async () => {
-            const res = await axiosSecure(`/scholarships?searchText=${searchText}`)
+            const res = await axiosSecure.get(`/scholarships`,{params:{searchText, ...filters}})
             return res.data
         }
     })
-    
+  
 
     return (
         <div className='mt-10  mx-10'>
             <h1 className='text-4xl font-bold text-purple-500 text-center '>All Scholarships</h1>
             <p className='text-xl font-bold text-center my-4'>Explore Your Dream University to Apply</p>
-            
-            <label className="input my-3">
+
+            <div className='flex flex-row justify-between mx-2 md:mx-a10 '>
+                {/* for search functionality */}
+                <label className="input my-3">
                 <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <g
                         strokeLinejoin="round"
@@ -38,9 +39,24 @@ const AllScholarships = () => {
                     </g>
                 </svg>
                 <input
-                onChange={(e)=>setSearchText(e.target.value)}
-                type="search" required placeholder="Search" />
+                    onChange={(e) => setSearchText(e.target.value)}
+                    type="search" required placeholder="Search" />
             </label>
+            {/* server side filter functionality */}
+            <select
+                className="select select-bordered"
+                onChange={(e) =>
+                    setFilters({ ...filters, degree: e.target.value })
+                }
+            >
+                <option value="">All </option>
+                <option value="Bachelor">Bachelor</option>
+                <option value="Masters">Masters</option>
+                <option value="Graduation">Graduation</option>
+                <option value="Diploma">Diploma</option>
+                
+            </select>
+            </div>
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                 {
